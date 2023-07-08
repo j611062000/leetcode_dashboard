786c212507992022f5b431bf8ab73b2c8c1a7333
sidlin <j611062000@gmail.com>
2023-07-09 02:44:59 +0700

    add tables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,46 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
+import Badge from 'react-bootstrap/Badge';
+import Button from '@mui/material/Button';
+import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { DataGrid } from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
-function getProblems(problemSet) {
+const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
+
+function BoxSx() {
   return (
-    <div>
-      {JSON.stringify(problemSet[1])}
-    </div>
-  )
+    <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Button>Save</Button>
+    </Box>
+  );
 }
 
-function App() {
-  const [problemSet, setProblemSet] = useState({"problems": []})
+function BasicExampleDataGrid() {
+  const { data } = useDemoData({
+    dataSet: 'Employee',
+    visibleFields: VISIBLE_FIELDS,
+    rowLength: 100,
+  });
 
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/problems', { mode: 'cors' , contentType: 'application/json'})
+  return (
+    <div style={{ height: '500', width: '100%' }}>
+      <DataGrid {...data} />
+    </div>
+  );
+}
+
+
+
+function App() {
+  const [problemSet, setProblemSet] = React.useState({ "problems": [] })
+
+  React.useEffect(() => {
+    fetch('http://127.0.0.1:5000/problems', { mode: 'cors', contentType: 'application/json' })
       .then(response => response.json())
       .then(responseJson => {
         setProblemSet(responseJson)
@@ -24,11 +51,15 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {problemSet ? getProblems(problemSet) : "Loading..."}
-      </header>
-    </div>
+      <div >
+        <Container>
+
+        </Container>
+        <Container>
+        {BasicExampleDataGrid()}
+
+        </Container>
+      </div>
   );
 }
 
--- makefile
+++ makefile
@@ -1,8 +1,11 @@
-start:
+start_backend:
 	python3 -m flask --app ./main.py run
 
-start_debug:
+start_backend_debug:
 	python3 -m flask --app ./main.py --debug run
+
+start_frontend:
+	cd frontend && npm start
 
 start_venv:
 	source ./env/bin/activate
